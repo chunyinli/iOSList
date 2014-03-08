01cd02165a1d3149ecf94632a0fde212bb7ddbdb
Chunyin Li <Chunyin_Li@epam.com>
2014-03-08 11:19:48 -0500

    bind window scrolling instead of and elemnt's scroll event. Add additional check to see if we are scrolling within the container. Fixed plugin init call so that it actually works.

--- js/jquery.ioslist-1.0.1.src.js
+++ js/jquery.ioslist-1.0.1.src.js
@@ -1,87 +1,122 @@
 (function($) {
-	var defaults = {
-		groupContainer: "dl",
-		groupHeader: "dt",
-		stationaryHeaderClass: "fakeHeader",
-		stationaryHeaderElement: "h2"
-	};
-	methods = {};
-	_methods = {
-		init: function(options) {
-			var $listWrapper, $fakeHeader, $listContainer, elems = [],
-				options = $.extend(defaults, options),
-				isIOS = navigator.userAgent.match(/ipad|iphone|ipod/gi) ? true : false;
-			$(this).wrap("<div class='listWrapper' data-ios='" + isIOS + "'></div>");
-			$listContainer = $(this);
-			$listWrapper = $(this).parent();
-			$listWrapper.prepend("<" + options.stationaryHeaderElement + "/>");
-			$fakeHeader = $listWrapper.find(options.stationaryHeaderElement).eq(0);
-			$fakeHeader.addClass(options.stationaryHeaderClass);
+    var defaults = {
+        groupContainer: "dl",
+        groupHeader: "dt",
+        stationaryHeaderClass: "fakeHeader",
+        stationaryHeaderElement: "h2"
+    };
+    methods = {};
+    _methods = {
+        init: function(options) {
+            var $listWrapper;
+            var $fakeHeader;
+            var $listContainer;
+            var elems = [];
+            var options = $.extend(defaults, options);
+            // var isIOS = navigator.userAgent.match(/ipad|iphone|ipod/gi) ? true : false;
 
-			$listContainer.find(options.groupContainer).each(function(index, elem) {
-				var $tmp_list = $listContainer.find(options.groupContainer).eq(index),
-					$tmp_header = $tmp_list.find(options.groupHeader).eq(0),
-					$tmp_listHeight = $tmp_list.height(),
-					$tmp_listOffset = $tmp_list.position().top;
-				elems.push({
-					"list": $tmp_list,
-					"header": $tmp_header,
-					"listHeight": $tmp_listHeight,
-					"headerText": $tmp_header.text(),
-					"headerHeight": $tmp_header.outerHeight(),
-					"listOffset": $tmp_listOffset,
-					"listBottom": $tmp_listHeight + $tmp_listOffset
-				});
-			});
+            $listContainer = $(this);
+            // $listContainer.wrap("<div class='listWrapper' data-ios='" + isIOS + "'></div>");
+            $listContainer.wrap("<div class='listWrapper'></div>");
+            $listWrapper = $listContainer.parent();
+            $listWrapper.prepend("<" + options.stationaryHeaderElement + "/>");
+            $fakeHeader = $listWrapper.find(options.stationaryHeaderElement).eq(0);
+            $fakeHeader.addClass(options.stationaryHeaderClass);
 
-			$fakeHeader.text(elems[0].headerText);
 
-			$listContainer.scroll(function() {
-				testPosition();
-			});
 
-			function testPosition() {
-				var currentTop = $listContainer.scrollTop(),
-					topElement, offscreenElement, topElementBottom, i = 0;
+            $listContainer.find(options.groupContainer).each(function(index, elem) {
+                var $tmp_list = $(this);
+                var $tmp_header = $tmp_list.find(options.groupHeader).eq(0);
 
-				while ((elems[i].listOffset - currentTop) <= 0) {
-					topElement = elems[i];
-					topElementBottom = topElement.listBottom - currentTop;
-					if (topElementBottom < -topElement.headerHeight) {
-						offscreenElement = topElement;
-					}
-					i++;
-					if (i >= elems.length) {
-						break;
-					}
-				}
+                    // keep in mind this doesn't include the padding-top
+                var $tmp_listHeight = $tmp_list.height();
+                var $tmp_listTop = $tmp_list.offset().top;
 
-				if (topElementBottom < 0 && topElementBottom > -topElement.headerHeight) {
-					$fakeHeader.addClass("hidden");
-					$(topElement.list).addClass("animated");
-				} else {
-					$fakeHeader.removeClass("hidden");
-					if (topElement) {
-						$(topElement.list).removeClass("animated");
-					}
-				}
+                elems.push({
+                    "list": $tmp_list,
+                    "header": $tmp_header,
+                    "listHeight": $tmp_listHeight,
+                    "headerText": $tmp_header.text(),
+                    "headerHeight": $tmp_header.outerHeight(),
+                    "listTop": $tmp_listTop,
+                    "listBottom": $tmp_listHeight + $tmp_listTop
+                });
+            });
 
-				if (topElement) {
-					$fakeHeader.text(topElement.headerText);
-				}
-			}
-		}
-	};
+            $fakeHeader.text(elems[0].headerText);
 
-	$.fn.ioslist = function(method) {
-		if (methods[method]) {
-			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-		} else if (typeof method === "object" || !method) {
-			return $.each(this, function() {
-				_methods.init.apply(this, arguments);
-			});
-		} else {
-			$.error("Method " + method + " does not exist on jquery.ioslist");
-		}
-	};
+            $(window).scroll(function() {
+                testPosition();
+            });
+
+            function testPosition() {
+
+                if(currentTop >= containerTop && currentTop < containerBottom) {
+	                var currentTop = $(window).scrollTop();
+	                var topElement;
+	                var offscreenElement;
+	                var elementRelativeBottom;
+	                var i = 0;
+	                var elementTop;
+	                var elementHeight;
+	                var headerHeight;
+	                var containerTop = $listContainer.offset().top;
+	                var containerBottom = containerTop + $listContainer.height();
+
+                    // while the element's top is passed
+                    while ((elems[i].list.offset().top - currentTop) <= 0) {
+
+                        topElement = elems[i];
+                        elementTop = topElement.list.offset().top;
+                        elementHeight = topElement.list.height();
+                        headerHeight = topElement.header.outerHeight();
+
+                        elementRelativeBottom = (elementHeight + elementTop) - currentTop;
+
+                        if (elementRelativeBottom < -headerHeight) {
+                            offscreenElement = topElement;
+                        }
+                        i++;
+                        if (i >= elems.length) {
+                            break;
+                        }
+                    }
+
+                    // hide fake header at the end of each list, within the bottom [headerHeight]px
+                    if (elementRelativeBottom < 0 && elementRelativeBottom > -headerHeight) {
+                        $fakeHeader.removeClass("show");
+                        topElement.list.addClass("animated");
+                    } else {
+                        $fakeHeader.addClass("show");
+                        if (topElement) {
+                            topElement.list.removeClass("animated");
+                        }
+                    }
+
+                    if (topElement) {
+                        $fakeHeader.text(topElement.headerText);
+                    }
+
+                } else {
+                    $fakeHeader.removeClass("show");
+
+                }
+
+                
+            }
+        }
+    };
+
+    $.fn.ioslist = function(arg) {
+        if (methods[arg]) {
+            return methods[arg].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof arg === "object" || !arg) {
+            return $.each(this, function() {
+                _methods.init.call(this, arg);
+            });
+        } else {
+            $.error("Method " + method + " does not exist on jquery.ioslist");
+        }
+    };
 })(jQuery);
